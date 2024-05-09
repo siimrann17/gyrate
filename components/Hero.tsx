7c0e7c2b538b6ad17ba921cd991639939bea3252
Simran Dev <simran1749dev@gmail.com>
2024-05-09 13:14:23 +0530

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import Button from "./Button";
+
+const Hero = () => {
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map" />
+
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        {/* <Image
+          src="/img1.jpg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+        /> */}
+        <h1 className="bold-52 lg:bold-88">Ride with Gyrate !</h1>
+        <p className="regular-16 mt-6 text-black-30 xl:max-w-[520px]">
+          Discover the joy of safe and carefree rides on e-scooters with Gyrate!
+          Our company is committed to promoting both safety and happiness. With
+          cutting-edge features and meticulous design, our e-scooters offer a
+          smooth and secure journey through city streets or scenic paths.
+          Experience the thrill of urban mobility without compromising on
+          safety. Join us at Gyrate and embark on a journey where every ride is
+          a delightful adventure.
+        </p>
+
+        {/* <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </div>
+
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">
+              Excellent Reviews
+            </span>
+          </p>
+        </div> */}
+
+        <div className="flex justify-center text-2xl">
+          {/* <Button type="button" title="Download App" variant="btn_green" /> */}
+          <Button
+            type="button"
+            title="How we work?"
+            icon="/play.svg"
+            variant="btn_white_text"
+          />
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[368px] flex-col gap-8 rounded-3xl bg-black px-8 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-white">Enter Date and Time</p>
+              {/* <Image src="/close.svg" alt="close" width={24} height={24} /> */}
+            </div>
+            <form>
+              <input
+                type="text"
+                className="text-10l border-zinc-800 border-2 ml-8 m-5 px-4 py-2"
+                placeholder="Enter Date"
+              />
+              <input
+                type="text"
+                className="text-10l border-zinc-800 border-2 ml-8 mt-1 m-5 px-4 py-2"
+                placeholder="Enter Time"
+              />
+            </form>
+            <button className="border-black border-2 bg-white text-black tex t-5l font-bold rounded ">
+              Proceed
+            </button>
+            {/* <p className="bold-20 text-white">Aguas Calientes</p> */}
+          </div>
+
+          {/* <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">173.28 mi</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">2.040 km</p>
+            </div>
+          </div> */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ constants/index.ts
@@ -0,0 +1,85 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'How Gyrate Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Affordable Rides',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      "CONTENT"
+  },
+  {
+    title: 'Set a schedule',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "CONTENT",
+  },
+  {
+    title: 'Right Smart Ride Green',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'CONTENT',
+  },
+  {
+    title: 'Many new locations everyday',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'CONTENT',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Gyrate',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Ride', 'e-scooters'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'gyrate@gmail.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+  ],
+};
